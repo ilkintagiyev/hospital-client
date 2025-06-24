b5e1187a51b3400630e1402c66670a191bb6ed9a
ilkintagiyev <taghiyevilkin41@gmail.com>
2025-06-24 18:44:35 +0400

    params fixed

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -3,7 +3,7 @@ import api from '@/constants/api'
 import Main from '@/layout/Main'
 import React from 'react'
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: any) => {
     const service = await api.get(`/services/${params.id}`)
 
     return (
